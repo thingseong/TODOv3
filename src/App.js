1f489026a0385e766176e757ee798a3990f8a63b
thingseong <audtjd1893@gmail.com>
2022-03-23 21:37:48 +0900

    check기능 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Todolist from './component/Todolist';
-import {DragDropContext} from 'react-beautiful-dnd';
 
 
 
 function App() {
   return (
     <div className="App">
-
         <Todolist></Todolist>
-
     </div>
   );
 }
